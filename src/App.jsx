8d5e4b19afc5364f2ea345cf12c61c2198397573
sunways <sashaosk2006@gmail.com>
2025-12-03 22:22:45 +0700

    add welcome screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useSelector, useDispatch } from 'react-redux'
-import { getIsAuth, getCurrentUser} from './app/store/selectors'
-import {login, logout} from './features/auth/authSlice'
+// src/App.jsx
+import React from 'react';
+import './App.css';
+import WelcomePage from './pages/WelcomePage/WelcomePage';
 
 function App() {
-  const dispatch = useDispatch();
-  const isAuth = useSelector(getIsAuth);
-  const user = useSelector(getCurrentUser);
-  const handleLogin = () => {
-    dispatch(
-      login({
-        user: { email: 'Alex_MM@gmail.com'},
-        token: 'fake-token-123',
-            })
-    );
-  };
-
-const handleLogout = () => {
-    dispatch(logout());
-  };
-
-  return (
-    <div style={{padding: 10}}>
-      <h5>Авторизция</h5>
-
-      <p>Авторизован: {isAuth ? 'Да' : 'Нет'}</p>
-      <p>Пользователь: {user ? user.email : 'Нет'}</p>
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout} style={{ marginLeft: 8 }}>
-        Logout
-      </button>
-    </div>
-  );
+  return <WelcomePage />;
 }
 
-export default App
+export default App;
