dc7af16c3741453caf36ca161aa6b191791d8148
Vadim Popov <vadimpopov10102005@gmail.com>
2026-01-16 17:03:05 +0700

    Мои локальные изменения перед обновлением

--- src/app/AppRouter.jsx
+++ src/app/AppRouter.jsx
@@ -1,15 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import WelcomePage from "../pages/WelcomePage/WelcomePage";
 import LoginPage from "../pages/LoginPage/LoginPage";
-import HomePage from "../pages/HomePage/HomePage";
+// import HomePage from "../pages/HomePage/HomePage";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<WelcomePage />} />
       <Route path="/login" element={<LoginPage />} />
-
-      <Route path="/home" element={<HomePage />} />
 
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
